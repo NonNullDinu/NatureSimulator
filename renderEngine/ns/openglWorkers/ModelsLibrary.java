2ef2b2485b584d1091ddd4e46155842bb108a3be
NonNullDinu <39565527+NonNullDinu@users.noreply.github.com>
2018-05-23 20:44:22 +0300

    Update ModelsLibrary.java

--- renderEngine/ns/openglWorkers/ModelsLibrary.java
+++ renderEngine/ns/openglWorkers/ModelsLibrary.java
@@ -9,6 +9,9 @@ import obj.OBJLoader;
 public class ModelsLibrary {
 	private static Map<String, VAO> models = new HashMap<>();
 	
+	/**
+	 * Intended to be called by the secondary thread, but it's not that much of a difference because @see DataPacking#createVAO
+	 */
 	public static VAO createEmptyVAO(String objFile) {
 		return getModel(objFile);
 	}
