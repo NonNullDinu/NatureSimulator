8f2e6752f75e4cf8bf520a21d02ec9db43fa24d2
NonNullDinu <39565527+NonNullDinu@users.noreply.github.com>
2018-05-24 20:21:47 +0300

    The utils
    The utils for creating .tex game-readable image files from .png files, and for creating .aud game-readable audio files from .wav audio files

---
+++ utils/ns/converting/PngToTexConvertor.java
@@ -0,0 +1,37 @@
+package ns.converting;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import res.Resource;
+
+public class PngToTexConvertor {
+	public static void main(String[] args) throws IOException {
+		byte[] buf = new byte[50];
+		int len = System.in.read(buf);
+		String location = "";
+		for (int i = 0; i < len - 1; i++)
+			location += (char) buf[i];
+		File target = new File("resources/" + location.replace(".png", ".tex"));
+		System.out.println("resources/" + location.replace(".png", ".tex"));
+		target.createNewFile();
+		BufferedWriter writer = new BufferedWriter(
+				new FileWriter(target));
+		BufferedImage img = ImageIO.read(new Resource(location).asInputStream());
+		int width = img.getWidth();
+		int height = img.getHeight();
+		writer.write(width + " " + height + "\n");
+		for(int y = height - 1; y >= 0; y--) {
+			for(int x = 0; x < width; x++) {
+				writer.write((x == 0 ? "" : " ") + img.getRGB(x, y));
+			}
+			writer.write((int) '\n');
+		}
+		writer.close();
+	}
+}
---
+++ utils/ns/converting/WavToAudConvertor.java
@@ -0,0 +1,35 @@
+package ns.converting;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.lwjgl.util.WaveData;
+
+import res.Resource;
+
+public class WavToAudConvertor {
+
+	public static void main(String[] args) throws IOException {
+		byte[] buf = new byte[50];
+		int len = System.in.read(buf);
+		String location = "";
+		for (int i = 0; i < len - 1; i++)
+			location += (char) buf[i];
+		File target = new File("resources/" + location.replace(".wav", ".aud"));
+		System.out.println("resources/" + location.replace(".wav", ".aud"));
+		target.createNewFile();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(target));
+		WaveData data = WaveData.create(new Resource(location).asInputStream());
+		int format = data.format;
+		ByteBuffer bufferData = data.data;
+		int samplerate = data.samplerate;
+		writer.write(format + " " + bufferData.limit() + " " + samplerate + '\n');
+		while(bufferData.hasRemaining()) {
+			writer.write(bufferData.get() + (bufferData.position() == bufferData.capacity() ? "" : " "));
+		}
+		writer.close();
+	}
+}
